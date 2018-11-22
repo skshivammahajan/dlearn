96556a5080067a5c4270ac6f209b81fd4223a8d3
skshivammahajan <skshivammahajan@gmail.com>
2018-11-22 14:26:43 +0530

    Adding git ignore

--- trauto/models.py
+++ trauto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class NodeInventory(models.Model):
+    node_name = models.CharField(max_length = 100)
+    node_ip = models.CharField(max_length = 30)
+    oss_ip = models.CharField(max_length = 30)
+    domain = models.
